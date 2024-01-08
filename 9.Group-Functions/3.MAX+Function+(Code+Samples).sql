7cc8184e3b4c08797672cd2ecec9391c7a828780
Vu Dinh Quang <vudinhquangk53@gmail.com>
2024-01-08 11:26:10 +0900

    MAX Function

---
+++ 9.Group-Functions/3.MAX+Function+(Code+Samples).sql
@@ -0,0 +1,9 @@
+/************* The Complete Oracle SQL Bootcamp *************
+ * Author  : Oracle Master Training                         *
+ * Course  : The Complete Oracle SQL Bootcamp               *
+ * Lecture : MAX Function 									*
+ ************************************************************/
+
+SELECT max(salary), max(hire_date), max(first_name) FROM employees;
+
+SELECT * FROM employees ORDER BY first_name;
